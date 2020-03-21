0daee5c5a497177c9eef44ad99734d9ab14ff70c
hotact <admin@btsan.com>
2020-03-21 20:57:51 +0800

    change wsHanle fn

--- impl/wsHandle.go
+++ impl/wsHandle.go
@@ -19,12 +19,30 @@ var upgrader=websocket.Upgrader{
 func WsHandle(w http.ResponseWriter, r *http.Request) {
 	var (
 		err error
-		dataJson string
-		wsConn *websocket.Conn
 		conn *Connection
-		message []byte
 	)
 	//普通 HTTP请求
+	if err=httpRequest(w,r);err!=nil {
+		return
+	}
+	if conn,err=wsRequest(w,r);err!=nil {
+		return
+	}
+	go httpOtherRequest(conn)
+	for {
+		//超时设置
+		if err=wsRequestDone(conn);err!=nil {
+			goto Err
+		}
+	}
+Err:
+	conn.Close()
+}
+//ws地址的普通http请求 包括数据验证
+func httpRequest(w http.ResponseWriter,r *http.Request) (err error) {
+	var (
+		dataJson string
+	)
 	if r.Header.Get("Connection")!="Upgrade" {
 		dataJson,err=HttpAuth(r)
 		if err!=nil {
@@ -32,13 +50,18 @@ func WsHandle(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		w.Write([]byte(dataJson))
-		return
 	}
+	return
+}
+//ws地址的ws请求 包括数据验证
+func wsRequest(w http.ResponseWriter,r *http.Request)(conn *Connection,err error)  {
+	var(
+		wsConn *websocket.Conn
+	)
 	if wsConn, err = upgrader.Upgrade(w, r, nil);err != nil{
 		log.Print("upgrade:", err)
 		return
 	}
-
 	if conn,err= BuildConn(wsConn);err!=nil {
 		conn.WsConn.WriteMessage(websocket.TextMessage,[]byte(err.Error()))
 		goto Err
@@ -49,25 +72,29 @@ func WsHandle(w http.ResponseWriter, r *http.Request) {
 		conn.WsConn.WriteMessage(websocket.TextMessage,[]byte(err.Error()))
 		goto Err
 	}
-	go func() {
-		for{
-			select {
-			case data:=<-HttpChan:
-				conn.WriteMsg(data)
-				fmt.Println("http推送",string(data))
-			}
-		}
-	}()
-	for {
-		//超时设置
-		conn.WsConn.SetReadDeadline(time.Now().Add(TIME_OUT))
-		if message, err=conn.ReadMsg() ;err!=nil{
-			log.Println("read:", err)
-			goto Err
-		}
-		conn.WriteMsg(message)
-		log.Printf("recv: %s", message)
-	}
 Err:
 	conn.Close()
+
+return
+}
+//来自其他http服务器的数据监听
+func httpOtherRequest(conn *Connection)  {
+	for{
+		select {
+		case data:=<-HttpChan:
+			conn.WriteMsg(data)
+			fmt.Println("http推送",string(data))
+		}
+	}
+}
+//数据验证通过之后的数据处理部分
+func wsRequestDone(conn *Connection ) (err error)  {
+	var message []byte
+	conn.WsConn.SetReadDeadline(time.Now().Add(TIME_OUT))
+	if message, err=conn.ReadMsg() ;err!=nil{
+		log.Println("read:", err)
+	}
+	conn.WriteMsg(message)
+	log.Printf("recv: %s", message)
+	return
 }
