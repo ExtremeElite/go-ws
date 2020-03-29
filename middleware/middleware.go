dbb04c0a940c9349cef3709b95442c91ba294b1c
hotact <admin@btsan.com>
2020-03-29 20:31:58 +0800

    日志

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -5,7 +5,7 @@ import (
 )
 
 type MiddleWare func(http.HandlerFunc) http.HandlerFunc
-
+//日志中间件
 func Loging() MiddleWare  {
 	return func(handlerFunc http.HandlerFunc) http.HandlerFunc {
 		return func(writer http.ResponseWriter, request *http.Request) {
