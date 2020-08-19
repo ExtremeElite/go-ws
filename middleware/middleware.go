44d04a41b4d8dcba19d18491cc9a84bd60c59ecb
Gorouting <gorouting@qq.com>
2020-08-19 17:53:04 +0800

    add cors

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -31,6 +31,16 @@ func Method(m string) Middleware {
 		}
 	}
 }
+func Cors() Middleware  {
+	return func(handlerFunc http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request){
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			w.Header().Set("Access-Control-Allow-Credentials", "true")
+			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+			w.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET")
+		}
+	}
+}
 func Use(fn http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
 	for _, middleware := range middlewares {
 		fn = middleware(fn)
