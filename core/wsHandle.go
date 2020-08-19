cda9448e0987fb78fa1e9d4e8ac3869d0a224cab
Gorouting <gorouting@qq.com>
2020-08-19 21:50:33 +0800

    del request

--- core/wsHandle.go
+++ core/wsHandle.go
@@ -1,12 +1,17 @@
 package core
 
 import (
+	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 	"time"
 	"ws/conf"
 )
-
+var upgrader=websocket.Upgrader{
+	CheckOrigin: func(r *http.Request) bool {
+		return true
+	},
+}
 func WsHandle(w http.ResponseWriter, r *http.Request) {
 	var (
 		err error
@@ -17,6 +22,10 @@ func WsHandle(w http.ResponseWriter, r *http.Request) {
 	//普通 HTTP请求
 	if r.Header.Get("Connection")!="Upgrade" {
 		w.Write([]byte(HELLO))
+		return
+	}
+	if conn,name,err=wsRequest(w,r);err!=nil {
+		log.Println("wsRequest:", err.Error())
 		return
 	}
 	AddNode(&Node{conn,name})
@@ -32,8 +41,7 @@ func WsHandle(w http.ResponseWriter, r *http.Request) {
 			conn.Close()
 		}
 }
-//ws地址的普通http请求 包括数据验证
-//数据验证通过之后的数据处理部分
+
 func wsRequestDone(conn *Connection ) (err error)  {
 	var message []byte
 	var wsTimeOut=conf.Config().Common.WsTimeOut
@@ -47,5 +55,20 @@ func wsRequestDone(conn *Connection ) (err error)  {
 		return
 	}
 	log.Printf("服务器收到的: %s\n", message)
+	return
+}
+//创建连接
+func wsRequest(w http.ResponseWriter,r *http.Request)(conn *Connection,name string,err error)  {
+	var(
+		wsConn *websocket.Conn
+	)
+	if wsConn, err = upgrader.Upgrade(w, r, nil);err != nil{
+		return
+	}
+	if conn,err= BuildConn(wsConn);err!=nil {
+		conn.WsConn.WriteMessage(websocket.TextMessage,[]byte(err.Error()))
+		conn.Close()
+		return
+	}
 	return
 }
--- request/ws.go
+++ request/ws.go
@@ -8,34 +8,7 @@ package request
 
 import (
 	"github.com/gorilla/websocket"
-	"log"
 	"net/http"
-	"ws/core"
-	"ws/middleware/auth"
 )
-var upgrader=websocket.Upgrader{
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-func WsRequest(w http.ResponseWriter,r *http.Request)(conn *core.Connection,name string,err error)  {
-	var(
-		wsConn *websocket.Conn
-	)
-	if wsConn, err = upgrader.Upgrade(w, r, nil);err != nil{
-		log.Print("upgrade:", err)
-		return
-	}
-	if conn,err= core.BuildConn(wsConn);err!=nil {
-		conn.WsConn.WriteMessage(websocket.TextMessage,[]byte(err.Error()))
-		conn.Close()
-		return
-	}
-	//登录判断
-	if name,err=auth.WsAuth(r);err!=nil {
-		log.Println(err.Error())
-		conn.WsConn.WriteMessage(websocket.TextMessage,[]byte(err.Error()))
-		conn.Close()
-	}
-	return
-}
+
+
