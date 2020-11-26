f4ac3bfdd180611f52fdee44f22fdd1e933fe921
Gorouting <gorouting@qq.com>
2020-11-26 16:39:46 +0800

    change middleware order

--- pipeLine/pipeLine.go
+++ pipeLine/pipeLine.go
@@ -8,6 +8,7 @@ import (
 )
 
 type Middleware func(http.HandlerFunc) http.HandlerFunc
+
 func Logging() Middleware {
 	return func(fn http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
@@ -20,20 +21,20 @@ func Logging() Middleware {
 	}
 }
 func Method(m string) Middleware {
-	m=strings.ToUpper(m)
+	m = strings.ToUpper(m)
 	return func(fn http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
 			if r.Method != m {
-				http.Error(w,`<h1>欢迎来到及时服务</h1>`, http.StatusBadRequest)
+				http.Error(w, `<h1>欢迎来到及时服务</h1>`, http.StatusBadRequest)
 				return
 			}
 			fn(w, r)
 		}
 	}
 }
-func Cors() Middleware  {
+func Cors() Middleware {
 	return func(handlerFunc http.HandlerFunc) http.HandlerFunc {
-		return func(w http.ResponseWriter, r *http.Request){
+		return func(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Access-Control-Allow-Origin", "*")
 			w.Header().Set("Access-Control-Allow-Credentials", "true")
 			w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
@@ -42,8 +43,9 @@ func Cors() Middleware  {
 	}
 }
 func Use(fn http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
-	for _, middleware := range middlewares {
-		fn = middleware(fn)
+	var middlewaresLen = len(middlewares) - 1
+	for key, _ := range middlewares {
+		fn = middlewares[middlewaresLen-key](fn)
 	}
 	return fn
 }
