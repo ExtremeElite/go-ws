1ee6210b851b70789bda1d39b4776be3ea80c48b
Gorouting <gorouting@qq.com>
2020-12-01 11:33:46 +0800

    change msg

--- pipeLine/auth.go
+++ pipeLine/auth.go
@@ -15,40 +15,42 @@ import (
 	"ws/db"
 )
 
-
+type Response struct {
+	Code int `json:"code"`
+	Msg string `json:"msg"`
+	Data interface{} `json:"data"`
+}
+func (response Response)Json(msg string,code int) string{
+	response.Msg=msg
+	response.Code=code
+	response.Data=""
+	data,err:=json.Marshal(response)
+	if err!=nil {
+		return `["code":404,"msg":"数据错误","data":""]`
+	}
+	return string(data)
+}
 type Login struct {
 	User string
 	Pwd string
 }
 var(
 	body       []byte
-	dataString string
 	login      Login
 )
 //ws登录
-func WsAuth(r *http.Request) (name string,err error)  {
+func wsAuth(r *http.Request) (name string,err error)  {
+	response:=Response{}
 	name,err=GetName(r)
 	if err!=nil{
 		return
 	}
 	if !validateToken(name) {
-		err=errors.New(`{"msg":"token失效","code":401}`)
+		err=errors.New(response.Json("token失效",http.StatusUnauthorized))
 	}
 	return
 }
-func GetName(r *http.Request) (name string,err error)  {
-	query:=r.URL.Query()
-	if len(query)==0 {
-		err=errors.New(`{"msg":"未获取到参数","code":401}`)
-		return
-	}
-	if token,ok:=query["token"];ok{
-		name=token[0]
-		return
-	}
-	err=errors.New(`{"msg":"请传入正确的参数","code":401}`)
-	return
-}
+
 //http登录
 func HttpAuth(r *http.Request)(data string,err error){
 	if r.Method=="GET" {
@@ -61,6 +63,21 @@ func HttpAuth(r *http.Request)(data string,err error){
 			return
 		}
 	}
+	return
+}
+
+func GetName(r *http.Request) (name string,err error)  {
+	response:=Response{}
+	query:=r.URL.Query()
+	if len(query)==0 {
+		err=errors.New(response.Json("未获取到参数",http.StatusUnauthorized))
+		return
+	}
+	if token,ok:=query["token"];ok{
+		name=token[0]
+		return
+	}
+	err=errors.New(response.Json("请传入正确的参数",http.StatusUnauthorized))
 	return
 }
 
@@ -77,7 +94,7 @@ func validateToken(token string) (ok bool)  {
 func WsAuthMiddle() Middleware {
 	return func(fn http.HandlerFunc) http.HandlerFunc {
 		return func(w http.ResponseWriter, r *http.Request) {
-			_,err:=WsAuth(r)
+			_,err:=wsAuth(r)
 			if err!=nil {
 				http.Error(w,err.Error(), http.StatusUnauthorized)
 				return
