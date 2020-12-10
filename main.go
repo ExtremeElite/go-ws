4b72898dec540edb5d4caa292868a8d52aa2fe0c
Gorouting <gorouting@qq.com>
2020-12-10 20:39:32 +0800

    change collor

--- main.go
+++ main.go
@@ -47,13 +47,6 @@ func Logo()  {
 	ascii := figlet4go.NewAsciiRender()
 	// Adding the colors to RenderOptions
 	options := figlet4go.NewRenderOptions()
-	options.FontColor = []figlet4go.Color{
-		// Colors can be given by default ansi color codes...
-		figlet4go.ColorGreen,
-		figlet4go.ColorYellow,
-		figlet4go.ColorCyan,
-	}
-
 	renderStr, _ := ascii.RenderOpts("Gorouting", options)
 	fmt.Println(renderStr)
 }
