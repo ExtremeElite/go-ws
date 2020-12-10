80a7b47b90c4091f0d6b0b8cd35cba98fa402e81
Gorouting <gorouting@qq.com>
2020-12-10 20:28:44 +0800

    gorouting logo

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/gorilla/websocket v1.4.2
+	github.com/mbndr/figlet4go v0.0.0-20190224160619-d6cef5b186ea
 	gorm.io/driver/mysql v1.0.3
 	gorm.io/gorm v1.20.7
 )
--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+	"github.com/mbndr/figlet4go"
 	"log"
 	"net/http"
 	"strconv"
@@ -12,6 +14,7 @@ import (
 
 func init()  {
 	broker.HttpChan =make(chan broker.PushData,1)
+	Logo()
 }
 func main() {
 	go httpPush()
@@ -40,4 +43,18 @@ func wsPush() {
 	}
 }
 
+func Logo()  {
+	ascii := figlet4go.NewAsciiRender()
+	// Adding the colors to RenderOptions
+	options := figlet4go.NewRenderOptions()
+	options.FontColor = []figlet4go.Color{
+		// Colors can be given by default ansi color codes...
+		figlet4go.ColorGreen,
+		figlet4go.ColorYellow,
+		figlet4go.ColorCyan,
+	}
+
+	renderStr, _ := ascii.RenderOpts("Gorouting", options)
+	fmt.Println(renderStr)
+}
 
