2a9d053350bc0ae6810a810342232189b7520b22
gorouting <gorouting@qq.com>
2022-06-13 23:28:30 +0800

    add create token

--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 
 require (
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/golang-jwt/jwt/v4 v4.4.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
--- pipeLine/jwt.go
+++ pipeLine/jwt.go
@@ -1,5 +1,37 @@
 package pipeLine
 
-import "ws/common"
+import (
+	"strconv"
+	"time"
+	"ws/common"
+
+	"github.com/golang-jwt/jwt"
+)
 
 var SignKey = []byte(common.Setting.SignKey)
+
+type CustomClaims struct {
+	userId      int
+	connectType int //1:ws 2:tcp 3:udp
+	jwt.StandardClaims
+}
+
+func CreateToken(id, connectType int) string {
+	maxAge := 60 * 60 * 24
+	customClaims := &CustomClaims{
+		userId:      id,
+		connectType: connectType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Add(time.Duration(maxAge) * time.Second).Unix(),
+			Issuer:    common.Setting.Name,
+			Id:        strconv.Itoa(id),
+		},
+	}
+	//采用HMAC SHA256加密算法
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, customClaims)
+	tokenString, err := token.SignedString(SignKey)
+	if err != nil {
+		common.LogInfoFailed(err.Error())
+	}
+	return tokenString
+}
